aafe8741e1049c985bb19c0cb37a4280cf3ab6f2
MortenKirch <morten-kris@live.dk>
2026-04-07 11:26:59 +0200

    Step one: remove boiler plate

---
+++ StickerSmash/app/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+    </View>
+  );
+}
