fa5105511d421f5e872ff5fd32d3ecc63a8d5546
MortenKirch <morten-kris@live.dk>
2026-04-10 09:36:52 +0200

    image added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
 import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 const styles = StyleSheet.create({
   container: {
@@ -13,11 +15,20 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#fff",
   },
+
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
 });
 
 export default function Index() {
   return (
     <View style={styles.container}>
+      <View>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
       <Text style={styles.text}>Home screen</Text>
       <Link href="/about">Go to About</Link>
     </View>
