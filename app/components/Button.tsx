23a52dc4aeaaefc1bcdcb8007d0723c4a6f3b556
MortenKirch <morten-kris@live.dk>
2026-04-10 12:13:27 +0200

    add expo image picker to handle picking images from phone library and displaying it in the app

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -3,10 +3,11 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 type Props = {
   label: string;
+  onPress?: () => void;
   theme?: "primary";
 };
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
   if (theme === "primary") {
     return (
       <View
@@ -17,7 +18,7 @@ export default function Button({ label, theme }: Props) {
       >
         <Pressable
           style={[styles.button, { backgroundColor: "#fff" }]}
-          onPress={() => alert("You pressed a button.")}
+          onPress={onPress}
         >
           <FontAwesome
             name="picture-o"
